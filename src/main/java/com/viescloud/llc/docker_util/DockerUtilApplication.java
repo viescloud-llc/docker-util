d3307a664f3f9af2ca4a724303981023478c585b
VincentB00 <vincentbui1999@gmail.com>
2025-07-21 02:13:20 -0400

    feat: Enable Feign Clients, JPA Repositories, Entity Scan and AspectJ AutoProxy

--- src/main/java/com/viescloud/llc/docker_util/DockerUtilApplication.java
+++ src/main/java/com/viescloud/llc/docker_util/DockerUtilApplication.java
@@ -2,7 +2,19 @@ package com.viescloud.llc.docker_util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.viescloud.eco.viesspringutils.auto.config.ViesBeanConfig;
+
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableFeignClients
+@EnableJpaRepositories
+@EntityScan
+@Import(ViesBeanConfig.class)
 @SpringBootApplication
 public class DockerUtilApplication {
 
