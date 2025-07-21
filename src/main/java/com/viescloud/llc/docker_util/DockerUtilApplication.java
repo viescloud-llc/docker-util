22a57929e1e5ba1f96f78b36d9247b7a0da3d897
VincentB00 <vincentbui1999@gmail.com>
2025-07-21 02:10:04 -0400

    feat: initial commit of docker-util project with Maven wrapper and configurations

---
+++ src/main/java/com/viescloud/llc/docker_util/DockerUtilApplication.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.docker_util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerUtilApplication.class, args);
+	}
+
+}
