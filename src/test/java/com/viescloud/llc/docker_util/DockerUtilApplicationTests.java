2b6d39ee5afe1bf2d04f7c437efcf4a0c79c2930
VincentB00 <vincentbui1999@gmail.com>
2025-08-18 12:45:09 -0400

    test: Remove unnecessary test annotation

--- src/test/java/com/viescloud/llc/docker_util/DockerUtilApplicationTests.java
+++ src/test/java/com/viescloud/llc/docker_util/DockerUtilApplicationTests.java
@@ -1,12 +1,7 @@
 package com.viescloud.llc.docker_util;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DockerUtilApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
